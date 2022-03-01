40e85dc0e755cf281bfe76732c850e64d3e39f0e
Bieldeid <gabrieldeid.android@gmail.com>
2022-03-01 20:24:55 -0300

    Navegação entre views realizada.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get ('/apartamentos', function(){
+    return view ('apartamentos');
+});
+
+Route::get ('/casas', function(){
+    return view ('casas');
+});
+
+Route::get ('/terrenos', function(){
+    return view ('terrenos');
+});
+
+Route::get ('/sobrados', function(){
+    return view ('sobrados');
+});
